343a91e4ac942c13d806276c49540fe28cdf3b38
sam phomsopha <samphomsopha@gmail.com>
2016-06-24 23:17:08 -0700

    Initial Commit

---
+++ UniversialLinkDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversialLinkDemo
+//
+//  Created by sam phomsopha on 6/24/16.
+//  Copyright © 2016 sam phomsopha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
